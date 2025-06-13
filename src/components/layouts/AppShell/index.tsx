48467baf0eac184cabab4740a3150d825b27fb31
asepsyaepull <asep.syaepul95@gmail.com>
2025-06-13 14:24:47 +0700

    fix: import Footer component and adjust layout in AppShell

--- src/components/layouts/AppShell/index.tsx
+++ src/components/layouts/AppShell/index.tsx
@@ -1,5 +1,6 @@
-import NavbarMenu from '@/components/molecules/Navbar/navbarMenu'
 import React from 'react'
+import NavbarMenu from '@/components/molecules/Navbar/navbarMenu'
+import Footer from '@/components/molecules/Footer/footer';
 
 type AppShellProps = {
     children: React.ReactNode;
@@ -11,6 +12,7 @@ const AppShell = (props:AppShellProps) => {
     <div>
         <NavbarMenu />
         {children}
+        <Footer />
     </div>
   )
 }
